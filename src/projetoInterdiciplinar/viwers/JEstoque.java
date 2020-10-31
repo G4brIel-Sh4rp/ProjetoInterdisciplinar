851a82fd4b4a7ce02913fa53792682181652ac93
Gabriel Pinheiro <gabriel.pds2001@gmail.com>
2020-10-31 20:53:17 -0300

    Criado Viwer para Estoque, Jtable para celular, loader instanciado dentro de estoque

--- src/projetoInterdiciplinar/viwers/JEstoque.java
+++ src/projetoInterdiciplinar/viwers/JEstoque.java
@@ -5,17 +5,49 @@
  */
 package projetoInterdiciplinar.viwers;
 
-/**
- *
- * @author Gabriel
- */
-public class JEstoque extends javax.swing.JFrame {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import  projetoInterdiciplinar.controler.Estoque;
+import  projetoInterdiciplinar.entities.Celulares;
+import  projetoInterdiciplinar.entities.Produto;
+import java.text.DecimalFormat;
 
-    /**
-     * Creates new form JEstoque
-     */
+public class JEstoque extends javax.swing.JFrame {
+    private Estoque etqCell = new Estoque("teste.csv");
+    
+    
     public JEstoque() {
         initComponents();
+        addRowTblCelulares();
+        tblCelulares.getColumnModel().getColumn(0).setPreferredWidth(30);
+        tblCelulares.getColumnModel().getColumn(1).setPreferredWidth(150);
+        tblCelulares.getColumnModel().getColumn(2).setPreferredWidth(150);
+        tblCelulares.getColumnModel().getColumn(3).setPreferredWidth(80);
+        tblCelulares.getColumnModel().getColumn(4).setPreferredWidth(100);
+        tblCelulares.getColumnModel().getColumn(5).setPreferredWidth(120);
+
+    }
+    public void addRowTblCelulares(){
+        SimpleDateFormat formatar = new SimpleDateFormat("dd/MM/yyyy");
+        DecimalFormat df = new DecimalFormat("#.00");
+        
+        DefaultTableModel modelo = (DefaultTableModel) tblCelulares.getModel();
+        ArrayList<Celulares> list = etqCell.listar();
+                          
+        Object rowData[] = new Object[6];
+        
+        for (int i = 0; i < list.size(); i++) {
+            rowData[0] = list.get(i).getId();
+            rowData[1] = list.get(i).getNome();
+            rowData[2] = list.get(i).getMarca();
+            rowData[3] = list.get(i).getQnt();
+            rowData[4] = "R$  "+ df.format(list.get(i).getPreco());
+            rowData[5] = formatar.format(list.get(i).getLancamento());
+            modelo.addRow(rowData);
+        }
+    
     }
 
     /**
@@ -27,18 +59,50 @@ public class JEstoque extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblCelulares = new javax.swing.JTable();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 610, Short.MAX_VALUE)
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(400, 250));
+        setPreferredSize(new java.awt.Dimension(900, 590));
+        getContentPane().setLayout(null);
+
+        tblCelulares.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "ID", "Modelo", "Marca", "Quantidade", "Preco", "Lançamento"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tblCelulares.setGridColor(new java.awt.Color(255, 255, 255));
+        tblCelulares.setRowHeight(20);
+        tblCelulares.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        jScrollPane1.setViewportView(tblCelulares);
+        tblCelulares.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 652, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 471, Short.MAX_VALUE)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 481, Short.MAX_VALUE)
         );
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 37, 652, 481);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -54,7 +118,7 @@ public class JEstoque extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Metal".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -79,5 +143,8 @@ public class JEstoque extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblCelulares;
     // End of variables declaration//GEN-END:variables
 }
